61e1ca69dd4a94a40b972be097247d01a3bbb782
rahul2546 <rs7545305@gmail.com>
2025-01-24 16:49:34 +0530

    implement user registration route and async error handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import exress from 'express';
+import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import e from 'express';
+
 
 
 const app = express();
@@ -16,5 +16,13 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
+
+
+// routes import
+
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+app.use('/api/v1/users', userRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import {app} from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
@@ -11,14 +11,12 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.on("error", (err) => { //Error handling related to the database and express communication
-        console.error("Error: ", err); 
-        throw err
-    })
+    
+    
     app.listen(process.env.PORT || 8000, () => {
-         console.log(`server is running on port ${process.env.PORT}`);
-    }
-)
+         console.log(`server is running on port : ${process.env.PORT}`);
+    })
+
 })
 .catch((err) => {
    console.log("MONGO db connection failed !!!", err);
